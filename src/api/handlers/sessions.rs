a3c7eeb9693817dd97e1ea4effa85c2e3e31949d
paulo <paulo.rmartins@gmail.com>
2023-01-30 22:09:21 +0000

    add sessions params from to

--- src/api/handlers/sessions.rs
+++ src/api/handlers/sessions.rs
@@ -29,12 +29,18 @@ use crate::records::EpochIndex;
 use actix_web::web::{Data, Json, Path, Query};
 use log::warn;
 use redis::aio::Connection;
-use serde::Deserialize;
+use serde::{de::Deserializer, Deserialize};
 
 #[derive(Debug, Deserialize, Clone, PartialEq)]
 pub struct Params {
     #[serde(default = "default_number_last_sessions")]
     number_last_sessions: u32,
+    #[serde(default)]
+    #[serde(deserialize_with = "parse_session")]
+    from: EpochIndex,
+    #[serde(default)]
+    #[serde(deserialize_with = "parse_session")]
+    to: EpochIndex,
     // show_stats indicates whether session stats should be retrieved or not, default false
     #[serde(default)]
     show_stats: bool,
@@ -42,6 +48,17 @@ pub struct Params {
 
 fn default_number_last_sessions() -> u32 {
     48
+}
+
+fn parse_session<'de, D>(d: D) -> Result<EpochIndex, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    Deserialize::deserialize(d).map(|x: Option<_>| {
+        x.unwrap_or("".to_string())
+            .parse::<EpochIndex>()
+            .unwrap_or_default()
+    })
 }
 
 /// Get a sessions filtered by query params
@@ -58,10 +75,22 @@ pub async fn get_sessions(
         .map_err(CacheError::RedisCMDError)?;
 
     let mut data: Vec<SessionResult> = Vec::new();
-    let mut last = Some(current_session - params.number_last_sessions);
-    while let Some(session_index) = last {
-        if session_index >= current_session {
-            last = None;
+
+    let (start_session, end_session) = if params.from != 0 && params.from < params.to {
+        (params.from, params.to)
+    } else if params.number_last_sessions != 0 {
+        (
+            current_session - params.number_last_sessions,
+            current_session - 1,
+        )
+    } else {
+        (current_session, current_session)
+    };
+
+    let mut i = Some(start_session);
+    while let Some(session_index) = i {
+        if session_index > end_session {
+            i = None;
         } else {
             let mut session_data: CacheMap = redis::cmd("HGETALL")
                 .arg(CacheKey::SessionByIndex(Index::Num(session_index.into())))
@@ -87,7 +116,7 @@ pub async fn get_sessions(
 
             data.push(session_data.into());
 
-            last = Some(session_index + 1);
+            i = Some(session_index + 1);
         }
     }
 
