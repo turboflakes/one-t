d203015762157e43296385544d6c71631c440dab
paulo <paulo.rmartins@gmail.com>
2023-03-01 23:32:29 +0000

    copy latest to polkadot and westend

--- src/onet.rs
+++ src/onet.rs
@@ -25,10 +25,9 @@ use crate::matrix::{Matrix, UserID, MATRIX_SUBSCRIBERS_FILENAME};
 use crate::records::EpochIndex;
 use crate::report::Network;
 use crate::runtimes::{
-    kusama,
-    // polkadot,
+    kusama, polkadot,
     support::{ChainPrefix, SupportedRuntime},
-    // westend,
+    westend,
 };
 use log::{debug, error, info, warn};
 use redis::aio::Connection;
@@ -232,9 +231,9 @@ impl Onet {
         self.cache_network().await?;
 
         match self.runtime {
-            SupportedRuntime::Polkadot => kusama::init_and_subscribe_on_chain_events(self).await,
+            SupportedRuntime::Polkadot => polkadot::init_and_subscribe_on_chain_events(self).await,
             SupportedRuntime::Kusama => kusama::init_and_subscribe_on_chain_events(self).await,
-            SupportedRuntime::Westend => kusama::init_and_subscribe_on_chain_events(self).await,
+            SupportedRuntime::Westend => westend::init_and_subscribe_on_chain_events(self).await,
             // _ => unreachable!(),
         }
     }
