5b9dddd77a92596d5d49fc502a676f0ad13e18c9
Paulo Martins <paulormart@users.noreply.github.com>
2025-06-28 10:41:36 +0100

    update asset hub westend metadata

--- update-metadata.sh
+++ update-metadata.sh
@@ -7,7 +7,7 @@
 # cargo install subxt-cli --force
 #
 # ****** Relay Chains ******
-# subxt metadata --url wss://rpc.turboflakes.io:443/westend -f bytes > packages/chains/westend/artifacts/metadata/westend_metadata.scale
+subxt metadata --url wss://rpc.turboflakes.io:443/westend -f bytes > packages/chains/westend/artifacts/metadata/westend_metadata.scale
 #subxt metadata --url wss://rpc.turboflakes.io:443/westend --pallets System,Session,Balances,Staking,Utility,NominationPools -f bytes > packages/chains/westend/artifacts/metadata/westend_metadata_small.scale
 # subxt metadata --url wss://rpc.turboflakes.io:443/kusama -f bytes > packages/chains/kusama/artifacts/metadata/kusama_metadata.scale
 #subxt metadata --url wss://rpc.turboflakes.io:443/kusama --pallets System,Session,Balances,Staking,Utility,NominationPools -f bytes > packages/chains/kusama/artifacts/metadata/kusama_metadata_small.scale
@@ -15,15 +15,15 @@
 #subxt metadata --url wss://rpc.turboflakes.io:443/polkadot --pallets System,Session,Balances,Staking,Utility,NominationPools -f bytes > packages/chains/polkadot/artifacts/metadata/polkadot_metadata_small.scale
 # subxt metadata --url wss://rpc.turboflakes.io:443/paseo -f bytes > packages/chains/paseo/artifacts/metadata/paseo_metadata.scale
 #subxt metadata --url wss://rpc.turboflakes.io:443/paseo --pallets System,Session,Balances,Staking,Utility,NominationPools,Identity -f bytes > packages/chains/paseo/artifacts/metadata/paseo_metadata_small.scale
-subxt metadata --url wss://dev-relay.turboflakes.io:443 -f bytes > packages/chains/westend-next/artifacts/metadata/westend_next_metadata.scale
+#subxt metadata --url wss://dev-relay.turboflakes.io:443 -f bytes > packages/chains/westend-next/artifacts/metadata/westend_next_metadata.scale
 # ****** People Chains ******
-# subxt metadata --url wss://sys.turboflakes.io:443/people-westend -f bytes > packages/chains/people-westend/artifacts/metadata/people_westend_metadata.scale
+subxt metadata --url wss://sys.turboflakes.io:443/people-westend -f bytes > packages/chains/people-westend/artifacts/metadata/people_westend_metadata.scale
 # subxt metadata --url wss://sys.turboflakes.io:443/people-kusama -f bytes > packages/chains/kusama/artifacts/metadata/people_kusama_metadata.scale
 # subxt metadata --url wss://sys.turboflakes.io:443/people-polkadot -f bytes > packages/chains/polkadot/artifacts/metadata/people_polkadot_metadata.scale
 # subxt metadata --url wss://sys.turboflakes.io:443/people-paseo -f bytes > packages/chains/paseo/artifacts/metadata/people_paseo_metadata.scale
 # ****** AssetHub Chains ******
-# subxt metadata --url wss://sys.turboflakes.io:443/asset-hub-westend -f bytes > packages/chains/asset-hub-westend/artifacts/metadata/asset_hub_westend_metadata.scale
-subxt metadata --url wss://dev-ah.turboflakes.io:443 -f bytes > packages/chains/asset-hub-westend-next/artifacts/metadata/asset_hub_westend_next_metadata.scale
+subxt metadata --url wss://sys.turboflakes.io:443/asset-hub-westend -f bytes > packages/chains/asset-hub-westend/artifacts/metadata/asset_hub_westend_metadata.scale
+#subxt metadata --url wss://dev-ah.turboflakes.io:443 -f bytes > packages/chains/asset-hub-westend-next/artifacts/metadata/asset_hub_westend_next_metadata.scale
 
 # Generate runtime API client code from metadata. (Development mode only)
 
@@ -31,6 +31,6 @@ subxt metadata --url wss://dev-ah.turboflakes.io:443 -f bytes > packages/chains/
 # subxt codegen --url wss://rpc.turboflakes.io:443/westend | rustfmt --edition=2018 --emit=stdout > packages/chains/westend/artifacts/metadata/westend_metadata.rs
 # subxt codegen --url wss://rpc.turboflakes.io:443/asset-hub-westend | rustfmt --edition=2018 --emit=stdout > packages/chains/asset-hub-westend/artifacts/metadata/asset_hub_westend_metadata.rs
 # subxt codegen --url wss://rpc.turboflakes.io:443/kusama | rustfmt --edition=2018 --emit=stdout > packages/chains/kusama/artifacts/metadata/kusama_runtime.rs
-subxt codegen --url wss://dev-relay.turboflakes.io:443 | rustfmt --edition=2018 --emit=stdout > packages/chains/westend-next/artifacts/metadata/westend_next_metadata.rs
-subxt codegen --url wss://dev-ah.turboflakes.io:443 | rustfmt --edition=2018 --emit=stdout > packages/chains/asset-hub-westend-next/artifacts/metadata/asset_hub_westend_next_metadata.rs
+#subxt codegen --url wss://dev-relay.turboflakes.io:443 | rustfmt --edition=2018 --emit=stdout > packages/chains/westend-next/artifacts/metadata/westend_next_metadata.rs
+#subxt codegen --url wss://dev-ah.turboflakes.io:443 | rustfmt --edition=2018 --emit=stdout > packages/chains/asset-hub-westend-next/artifacts/metadata/asset_hub_westend_next_metadata.rs
 ```
