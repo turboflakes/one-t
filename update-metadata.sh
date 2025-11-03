efaa17bfbe14b380955d762ae8ab0d551551985b
Paulo Martins <paulormart@users.noreply.github.com>
2025-11-03 17:51:48 +0000

    remove westend-next

--- update-metadata.sh
+++ update-metadata.sh
@@ -15,7 +15,6 @@ subxt metadata --url wss://rpc.turboflakes.io:443/kusama -f bytes > packages/cha
 #subxt metadata --url wss://rpc.turboflakes.io:443/polkadot --pallets System,Session,Balances,Staking,Utility,NominationPools -f bytes > packages/chains/polkadot/artifacts/metadata/polkadot_metadata_small.scale
 subxt metadata --url wss://rpc.turboflakes.io:443/paseo -f bytes > packages/chains/paseo/artifacts/metadata/paseo_metadata.scale
 #subxt metadata --url wss://rpc.turboflakes.io:443/paseo --pallets System,Session,Balances,Staking,Utility,NominationPools,Identity -f bytes > packages/chains/paseo/artifacts/metadata/paseo_metadata_small.scale
-#subxt metadata --url wss://dev-relay.turboflakes.io:443 -f bytes > packages/chains/westend-next/artifacts/metadata/westend_next_metadata.scale
 # ****** People Chains ******
 subxt metadata --url wss://sys.turboflakes.io:443/people-westend -f bytes > packages/chains/people-westend/artifacts/metadata/people_westend_metadata.scale
 subxt metadata --url wss://sys.turboflakes.io:443/people-kusama -f bytes > packages/chains/people-kusama/artifacts/metadata/people_kusama_metadata.scale
@@ -33,5 +32,4 @@ subxt metadata --url wss://sys.turboflakes.io:443/asset-hub-paseo -f bytes > pac
 # subxt codegen --url wss://rpc.turboflakes.io:443/asset-hub-westend | rustfmt --edition=2018 --emit=stdout > packages/chains/asset-hub-westend/artifacts/metadata/asset_hub_westend_metadata.rs
 # subxt codegen --url wss://rpc.turboflakes.io:443/kusama | rustfmt --edition=2018 --emit=stdout > packages/chains/kusama/artifacts/metadata/kusama_runtime.rs
 #subxt codegen --url wss://dev-relay.turboflakes.io:443 | rustfmt --edition=2018 --emit=stdout > packages/chains/westend-next/artifacts/metadata/westend_next_metadata.rs
-#subxt codegen --url wss://dev-ah.turboflakes.io:443 | rustfmt --edition=2018 --emit=stdout > packages/chains/asset-hub-westend-next/artifacts/metadata/asset_hub_westend_next_metadata.rs
 ```
